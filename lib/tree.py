00df7341c0c47a58e45624f4fcdfdc24a080b834
Edgar <edgarasige@gmail.com>
2024-06-13 22:09:56 +0300

    Completed assingments

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,19 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, id, traversal='depth'):
+    if traversal == 'depth':
+      stack = [self.root]
+      while stack:
+        node = stack.pop()
+        if node and node.get('id') == id:
+          return node
+        stack.extend(node.get('children', []))
+    elif traversal == 'breadth':
+      queue = [self.root]
+      while queue:
+        node = queue.pop(0)
+        if node and node.get('id') == id:
+          return node
+        queue.extend(node.get('children', []))
+    return None
